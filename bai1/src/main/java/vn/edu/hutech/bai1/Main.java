b28b27f55ee15a7362fa932f525a55473ba63d09
Sharlotte <66898063+sharrlotte@users.noreply.github.com>
2024-05-09 16:28:11 +0700

    bai1

---
+++ bai1/src/main/java/vn/edu/hutech/bai1/Main.java
@@ -0,0 +1,13 @@
+package vn.edu.hutech.bai1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Main {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+
+}
