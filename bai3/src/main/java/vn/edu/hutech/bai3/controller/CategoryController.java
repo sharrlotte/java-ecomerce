b5edd060c145b88b201d0b4e278e24c3b2e37052
Sharlotte <66898063+sharrlotte@users.noreply.github.com>
2024-05-09 18:25:52 +0700

    Bai 3

---
+++ bai3/src/main/java/vn/edu/hutech/bai3/controller/CategoryController.java
@@ -0,0 +1,74 @@
+package vn.edu.hutech.bai3.controller;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import vn.edu.hutech.bai3.model.Category;
+import vn.edu.hutech.bai3.service.CategoryService;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+public class CategoryController {
+
+    private final CategoryService categoryService;
+
+    @GetMapping("/categories/add")
+    public String showAddForm(Model model) {
+        model.addAttribute("category", new Category());
+        return "/categories/add-category";
+    }
+
+    @PostMapping("/categories/add")
+    public String addCategory(@Valid Category category, BindingResult result) {
+        if (result.hasErrors()) {
+            return "/categories/add-category";
+        }
+        categoryService.addCategory(category);
+        return "redirect:/categories";
+    }
+
+    @GetMapping("/categories")
+    public String listCategories(Model model) {
+        List<Category> categories = categoryService.getAllCategories();
+        model.addAttribute("categories", categories);
+        return "/categories/categories-list";
+    }
+
+    @GetMapping("/categories/edit/{id}")
+    public String showUpdateForm(@PathVariable("id") Long id, Model model) {
+        Category category = categoryService.getCategoryById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Invalid category Id:"
+                        + id));
+        model.addAttribute("category", category);
+        return "/categories/update-category";
+    }
+
+    @PostMapping("/categories/update/{id}")
+    public String updateCategory(@PathVariable("id") Long id, @Valid Category category,
+            BindingResult result, Model model) {
+        if (result.hasErrors()) {
+            category.setId(id);
+            return "/categories/update-category";
+        }
+
+        categoryService.updateCategory(category);
+        model.addAttribute("categories", categoryService.getAllCategories());
+        return "redirect:/categories";
+    }
+
+    @GetMapping("/categories/delete/{id}")
+    public String deleteCategory(@PathVariable("id") Long id, Model model) {
+        categoryService.getCategoryById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Invalid category Id:" + id));
+        categoryService.deleteCategoryById(id);
+        model.addAttribute("categories", categoryService.getAllCategories());
+        return "redirect:/categories";
+    }
+}
---
+++ bai3/src/main/java/vn/edu/hutech/bai3/model/Product.java
@@ -0,0 +1,35 @@
+package vn.edu.hutech.bai3.model;
+
+import org.hibernate.validator.constraints.Length;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+
+@RequiredArgsConstructor
+@Data
+@Entity
+@Table(name = "products")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Length(min = 1, max = 100)
+    private String name;
+
+    @Min(1)
+    @Max(10000)
+    private double price;
+
+    @NotBlank
+    @Length(min = 1, max = 1000)
+    private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private Category category;
+}
